0001c61a6f707bb86d35b650c030dd313233442e
theo feuerstose <theo.feu54@gmail.com>
2026-02-05 17:10:01 +0100

    add categorie model + new login.html.twig style

---
+++ app/models/CategorieModel.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\models;
+
+use App\Config\Database;
+use PDO;
+use PDOException;
+use RuntimeException;
+
+class CategorieModel
+{
+    private PDO $db;
+
+    public function __construct()
+    {
+        $this->db = Database::getInstance();
+    }
+
+    public function findByUsername(string $username): ?array
+    {
+        try {
+            $query = $this->db->prepare(
+                'SELECT id, nom, FROM categories'
+            );
+            $query->execute();
+            $categoriesData = $query->fetch(PDO::FETCH_ASSOC);
+        } catch (PDOException $e) {
+            error_log($e->getMessage());
+            throw new RuntimeException("Erreur lors de la récupération des catégories");
+        }
+
+        return $categoriesData ?: null;
+    }
+}
